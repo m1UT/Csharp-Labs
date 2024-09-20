5fdc19a41d0305b0888d2ac8cda8f829db4913c8
mIUT <matveikaok@gmail.com>
2024-09-21 01:20:37 +0300

    renamed:    Lab1_1.sln -> Csharp-Lab.sln 	modified:   Lab1_1/bin/Debug/net8.0/Lab1_1.dll 	modified:   Lab1_1/bin/Debug/net8.0/Lab1_1.pdb 	modified:   Lab1_1/obj/Debug/net8.0/Lab1_1.AssemblyInfo.cs 	modified:   Lab1_1/obj/Debug/net8.0/Lab1_1.AssemblyInfoInputs.cache 	modified:   Lab1_1/obj/Debug/net8.0/Lab1_1.dll 	modified:   Lab1_1/obj/Debug/net8.0/Lab1_1.pdb 	modified:   Lab1_1/obj/Debug/net8.0/Lab1_1.sourcelink.json 	modified:   Lab1_1/obj/Debug/net8.0/ref/Lab1_1.dll 	modified:   Lab1_1/obj/Debug/net8.0/refint/Lab1_1.dll 	new file:   Lab1_2/Lab1_2.csproj 	new file:   Lab1_2/Program.cs 	new file:   Lab1_2/bin/Debug/net8.0/Lab1_2 	new file:   Lab1_2/bin/Debug/net8.0/Lab1_2.deps.json 	new file:   Lab1_2/bin/Debug/net8.0/Lab1_2.dll 	new file:   Lab1_2/bin/Debug/net8.0/Lab1_2.pdb 	new file:   Lab1_2/bin/Debug/net8.0/Lab1_2.runtimeconfig.json 	new file:   Lab1_2/obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs 	new file:   Lab1_2/obj/Debug/net8.0/Lab1_2.AssemblyInfo.cs 	new file:   Lab1_2/obj/Debug/net8.0/Lab1_2.AssemblyInfoInputs.cache 	new file:   Lab1_2/obj/Debug/net8.0/Lab1_2.GeneratedMSBuildEditorConfig.editorconfig 	new file:   Lab1_2/obj/Debug/net8.0/Lab1_2.GlobalUsings.g.cs 	new file:   Lab1_2/obj/Debug/net8.0/Lab1_2.assets.cache 	new file:   Lab1_2/obj/Debug/net8.0/Lab1_2.csproj.CoreCompileInputs.cache 	new file:   Lab1_2/obj/Debug/net8.0/Lab1_2.csproj.FileListAbsolute.txt 	new file:   Lab1_2/obj/Debug/net8.0/Lab1_2.dll 	new file:   Lab1_2/obj/Debug/net8.0/Lab1_2.genruntimeconfig.cache 	new file:   Lab1_2/obj/Debug/net8.0/Lab1_2.pdb 	new file:   Lab1_2/obj/Debug/net8.0/Lab1_2.sourcelink.json 	new file:   Lab1_2/obj/Debug/net8.0/apphost 	new file:   Lab1_2/obj/Debug/net8.0/ref/Lab1_2.dll 	new file:   Lab1_2/obj/Debug/net8.0/refint/Lab1_2.dll 	new file:   Lab1_2/obj/Lab1_2.csproj.nuget.dgspec.json 	new file:   Lab1_2/obj/Lab1_2.csproj.nuget.g.props 	new file:   Lab1_2/obj/Lab1_2.csproj.nuget.g.targets 	new file:   Lab1_2/obj/project.assets.json 	new file:   Lab1_2/obj/project.nuget.cache

---
+++ Lab1_2/Program.cs
@@ -0,0 +1,14 @@
+﻿int n=1;
+double y=0;
+
+Console.Write("Введите x: ");
+Double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(Convert.ToString(x));
+
+while (n <= 20)
+{   
+    y = y + Math.Pow(x, n-1)/(2*n+1);
+    n ++;
+}
+
+Console.Write("Ответ: "+Convert.ToString(y));
