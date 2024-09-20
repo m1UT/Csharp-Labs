cbc738714789dfe246f4c14bec7368dd9259cbbc
mIUT <matveikaok@gmail.com>
2024-09-21 00:12:46 +0300

    new file:   .vscode/settings.json new file:   Lab1_1.sln new file:   Lab1_1/Lab1_1.csproj new file:   Lab1_1/Program.cs new file:   Lab1_1/bin/Debug/net8.0/Lab1_1 new file:   Lab1_1/bin/Debug/net8.0/Lab1_1.deps.json new file:   Lab1_1/bin/Debug/net8.0/Lab1_1.dll new file:   Lab1_1/bin/Debug/net8.0/Lab1_1.pdb new file:   Lab1_1/bin/Debug/net8.0/Lab1_1.runtimeconfig.json new file:   Lab1_1/obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs new file:   Lab1_1/obj/Debug/net8.0/Lab1_1.AssemblyInfo.cs new file:   Lab1_1/obj/Debug/net8.0/Lab1_1.AssemblyInfoInputs.cache new file:   Lab1_1/obj/Debug/net8.0/Lab1_1.GeneratedMSBuildEditorConfig.editorconfig new file:   Lab1_1/obj/Debug/net8.0/Lab1_1.GlobalUsings.g.cs new file:   Lab1_1/obj/Debug/net8.0/Lab1_1.assets.cache new file:   Lab1_1/obj/Debug/net8.0/Lab1_1.csproj.CoreCompileInputs.cache new file:   Lab1_1/obj/Debug/net8.0/Lab1_1.csproj.FileListAbsolute.txt new file:   Lab1_1/obj/Debug/net8.0/Lab1_1.dll new file:   Lab1_1/obj/Debug/net8.0/Lab1_1.genruntimeconfig.cache new file:   Lab1_1/obj/Debug/net8.0/Lab1_1.pdb new file:   Lab1_1/obj/Debug/net8.0/Lab1_1.sourcelink.json new file:   Lab1_1/obj/Debug/net8.0/apphost new file:   Lab1_1/obj/Debug/net8.0/ref/Lab1_1.dll new file:   Lab1_1/obj/Debug/net8.0/refint/Lab1_1.dll new file:   Lab1_1/obj/Lab1_1.csproj.nuget.dgspec.json new file:   Lab1_1/obj/Lab1_1.csproj.nuget.g.props new file:   Lab1_1/obj/Lab1_1.csproj.nuget.g.targets new file:   Lab1_1/obj/project.assets.json new file:   Lab1_1/obj/project.nuget.cache

---
+++ Lab1_1/Program.cs
@@ -0,0 +1,31 @@
+﻿double x, y, f=0, a;
+int n = 0;
+
+Console.Write("Введите X = ");
+string? s = Console.ReadLine();
+x = Convert.ToDouble(s);
+Console.Write("Введите Y = ");
+s = Console.ReadLine();
+y = Convert.ToDouble(s);
+
+Console.WriteLine("Выберите f:\n1 - x^2\n2 - e^x\n3 - sh(x)");
+s = Console.ReadLine();
+n = Convert.ToInt16(s);
+switch(n)
+{
+    case 1: f=Math.Pow(x,2); break;
+    case 2: f=Math.Exp(x); break;
+    case 3: f=Math.Sinh(x); break;
+}
+
+if (x*y>0)
+    a=Math.Pow(f+y, 2)-Math.Pow(Math.Abs(f), 1.0/3);
+else if (x*y<0)
+    a=Math.Pow(f+y, 2)+Math.Sin(x);
+else
+    a=Math.Pow(f+y, 2)+Math.Pow(y, 3);
+
+Console.WriteLine("Ответ: "+Convert.ToString(a));
+
+
+
